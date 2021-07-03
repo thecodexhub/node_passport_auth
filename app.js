82e3c42bf5cf3b8a7b48798bd6f95ed168225b16
Sandip <sandipramanik07@gmail.com>
2021-07-03 18:21:17 +0530

    Add express routes

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started at port ${PORT}`));
